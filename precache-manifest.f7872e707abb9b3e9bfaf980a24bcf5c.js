c3400983e894e9900c387a06514719fc2d1b3345
GeorgeNicola <nicola_george1999@yahoo.com>
2020-06-21 19:59:09 +0300

    Updates

---
+++ precache-manifest.f7872e707abb9b3e9bfaf980a24bcf5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9887711712159e5ebc2277aa93a05498",
+    "url": "/Snake/index.html"
+  },
+  {
+    "revision": "8aec05046d4091af6581",
+    "url": "/Snake/static/css/main.759482cb.chunk.css"
+  },
+  {
+    "revision": "b71d8e969c6365c5e527",
+    "url": "/Snake/static/js/2.539e2df3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Snake/static/js/2.539e2df3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aec05046d4091af6581",
+    "url": "/Snake/static/js/main.e7db98ae.chunk.js"
+  },
+  {
+    "revision": "7f650829b46b4335ad15",
+    "url": "/Snake/static/js/runtime-main.01a72eab.js"
+  }
+]);
